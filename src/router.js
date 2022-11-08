694c5d0e2bfa00b296eeaeab69e52925d5896aff
just4fun147 <pauluswindito1@gmail.com>
2022-11-08 07:20:08 +0700

    testUbah

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+function importComponent(path){
+    return () => import(`./components/${path}.vue`);
+}
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "admin",
+            component: importComponent("DashboardLayout"),
+            children: [
+                //Dashboard
+                {
+                    path: "/",
+                    name: "Root",
+                    component: importComponent("DashboardIndex"),
+                },
+
+                //To Do List
+                {
+                    path: "/gd",
+                    name: "Guided",
+                    component: importComponent("TodoList/ListItem"),
+                },
+            ],
+        },
+    ],
+});
+
+export default router;
